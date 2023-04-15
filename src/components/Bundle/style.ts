ff813d31298612a7d1a93341f82f3520b17e0edb
gabriel <gabrielmaranhao48@gmail.com>
2023-04-15 14:09:59 -0300

    feat: component  bundle

---
+++ src/components/Bundle/style.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+    background-image: url('src/assets/back.png');
+    width: 100%;
+    height: 548px;
+`
+export const Main = styled.main`
+    position: absolute;
+    left: 155px;
+    top: 234px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    #check_out {
+        letter-spacing: 0.08em;
+        display: flex;
+        flex-direction: column;
+    }
+    #paragraph_bundle{
+        width: 349px;
+        margin: 16px 0px 32px 0px;
+    }
+`
